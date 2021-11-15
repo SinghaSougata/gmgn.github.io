85b0ae38b1ee87b0e810312f78624a6ad7d28664
SinghaSougata <91247187+SinghaSougata@users.noreply.github.com>
2021-11-15 19:24:08 +0530

    gmgn

---
+++ index.js
@@ -0,0 +1,24 @@
+function pr() {
+    var e1 = document.getElementById("one");
+    e1.innerText = "Good Morning";
+    e1.style.color = "black";
+    var e2 = document.getElementById("two");
+    e2.innerText = "Sougata";
+    e2.style.color = "green";
+  }
+ function br() {
+    var e1 = document.getElementById("one");
+    e1.innerText = "Good Evening";
+    e1.style.color = "yellow";
+    var e2 = document.getElementById("two");
+    e2.innerText = "Sougata";
+    e2.style.color = "purple";
+  }
+ function cr() {
+    var e1 = document.getElementById("one");
+    e1.innerText = "Good Night";
+    e1.style.color = "orange";
+    var e2 = document.getElementById("two");
+    e2.innerText = "sougata";
+    e2.style.color = "pink";
+ }
